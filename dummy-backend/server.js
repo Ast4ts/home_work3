8b78b479c88e53443b904d1dc6d57ff42ff771ba
root <root@computer.localdomain>
2022-10-10 13:37:02 +0300

    update

---
+++ dummy-backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const { execSync } = require('child_process');
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+	  host: "192.168.150.88",
+	  user: "itea",
+	  password: "itea",
+    database: 'itea'
+});
+
+con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+});
+
+class HandlerGenerator {
+  async get_modules_count (req, res) {
+    let count;
+    let query = 'SELECT * FROM my_modules'
+    count = await con.query(query, (err, rows) => {
+      if(err) throw err;  
+      res.json({
+        count: rows.length
+      });
+    });
+  }
+}
+
+function main () {
+	let app = express();
+  let handlers = new HandlerGenerator();
+	const port = process.env.PORT || 8000;
+   app.use(function (req, res, next){
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', '*');
+   res.header('Access-Control-Allow-Credentials', 'true');
+next();  
+})
+	
+  app.get('/get_modules_count', handlers.get_modules_count);
+	var server = http.createServer(app);
+	server.listen(port, () => console.log(`Server is listening on port: ${port}`));
+}
+
+main();
