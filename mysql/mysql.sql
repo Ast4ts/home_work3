cb7bdf0cd09e96680c50874e6b2592eb619ed82a
root <root@computer.localdomain>
2022-10-10 13:16:33 +0300

    update

---
+++ mysql/mysql.sql
@@ -0,0 +1,5 @@
+use itea;
+CREATE TABLE my_modules (Module_id INT NOT NULL AUTO_INCREMENT, Module VARCHAR(20), PRIMARY KEY (Module_id));
+insert into my_modules (Module) VALUES ('GIT');
+insert into my_modules (Module) VALUES ('JENKINS');
+
